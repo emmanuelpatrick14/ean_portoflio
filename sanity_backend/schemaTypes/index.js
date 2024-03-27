feb13342236dbff1add2c8dcf953dfac78c00bb2
Emmanuel <emmanueljoeemie@gmail.com>
2024-03-27 18:28:08 +0100

    ...backend

---
+++ sanity_backend/schemaTypes/index.js
@@ -0,0 +1,11 @@
+import testimonials from './testimonials.js'
+import about from './about.js'
+import brands from './brands.js'
+import contact from './contact.js'
+import experiences from './experiences.js'
+import skills from './skills.js'
+import workExperience from './workExperience.js'
+import works from './works.js'
+
+
+export const schemaTypes = [testimonials,about,brands,contact,experiences,skills,works,workExperience]
