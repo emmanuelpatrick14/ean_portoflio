dac4875933763265d63f2d0852fa8d0d58da8716
Emmanuel <emmanueljoeemie@gmail.com>
2024-03-28 15:48:35 +0100

    complete hero section

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { images } from "../../contants";
+import { images } from "../../constants";
 import { HiMenuAlt4, HiX } from "react-icons/hi";
 import { motion } from "framer-motion";
 import "./Navbar.scss";
