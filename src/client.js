0ce20dfda2d9283524132e7ca2c7a0ae3197a2c0
Emmanuel <emmanueljoeemie@gmail.com>
2024-03-29 01:19:11 +0100

    complete the about and join the frontend to the bakend

---
+++ src/client.js
@@ -0,0 +1,17 @@
+import {createClient} from '@sanity/client'
+import imageUrlBuilder from '@sanity/image-url'
+
+export const client = createClient({
+  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+  dataset: 'production',
+  useCdn: true, // set to `false` to bypass the edge cache
+  apiVersion: '2023-05-03', // use current date (YYYY-MM-DD) to target the latest API version
+})
+
+const builder = imageUrlBuilder(client)
+
+export const urlFor = (source)=> builder.image(source)
+
+
+const data = await client.fetch(`count(*)`)
+console.log(`Number of documents: ${data}`)
--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,9 +1,50 @@
 import React from "react";
+import { motion } from "framer-motion";
+import { useState, useEffect } from "react";
+import {images} from "../../constants"
+
+
+import { urlFor,client } from "../../client";
 
 import "./About.scss";
 
+
 const About = () => {
-  return <div>Aboutdxdxdxddd</div>;
+  console.log("About");
+  const [abouts, setAbouts] = useState([])
+
+  useEffect(() => {
+		client
+			.fetch(
+				`*[_type == "abouts"]`
+			)
+			.then((data) => setAbouts(data))
+			.catch(console.error);
+	}, []);
+
+  return (
+    <>
+      <h2 className="head-text">
+        I Know That <span>Good Development</span> <br /> means <span> Good Business</span>
+      </h2>
+
+      <div className="app__profiles">
+    {abouts.map((about,index)=>(
+      <motion.div whileHover={{scale:1.1}}
+      whileInView={{opacity:1}}
+      transition={{duration:0.5,type:'tween'}}
+      className="app__profile-item"
+      key={`about.title+${index}`}
+      >
+        <img src={urlFor(about.imgUrl)} alt={about} />
+        <h2 className="bold-text" style={{marginTop:20}}>{about.title}</h2>
+        <p className="p-text" style={{marginTop:10}}>{about.description}</p>
+
+      </motion.div>
+    ))}
+      </div>
+    </>
+  );
 };
 
 export default About;
