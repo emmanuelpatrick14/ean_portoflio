aa876e01501d439a1840ab6f5b342caafe325fc5
Emmanuel <emmanueljoeemie@gmail.com>
2024-03-31 23:49:25 +0100

    complete the work section UI

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
-        <img src={images.logo} alt="logo" />
+        {/* <img src={images.logo} alt="logo" /> */}
       </div>
       <ul className="app__navbar-links">
         {["home", "about", "work", "contact", "skills"].map((item) => (
